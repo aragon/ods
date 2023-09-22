59464f8c882922beedc3fd4d3ba73baeadf48662
Ruggero Cino <ruggero.cino@gmail.com>
2023-09-22 16:47:19 +0200

    Enable no-custom-classname eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,6 @@ const rulesBase = {
     'brace-style': 'warn',
     'prefer-template': 'warn',
     'no-useless-concat': 'warn',
-    'tailwindcss/no-custom-classname': ['off'],
 };
 
 const typescriptRules = {
