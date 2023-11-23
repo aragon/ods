fc01c70ff652007225f3b4f8bd2dc8726694c1ca
Fabrice Francois <fabricevladimir@gmail.com>
2023-11-23 10:58:54 -0500

    avatar tests and other stories

--- src/components/avatars/avatar/avatar.stories.ts
+++ src/components/avatars/avatar/avatar.stories.ts
@@ -11,26 +11,24 @@ const meta: Meta<typeof Avatar> = {
 type Story = StoryObj<typeof Avatar>;
 
 /**
- * Default usage example of Avatar component.
+ * Default usage of the Avatar component
  */
 export const Default: Story = {
     args: {
-        responsiveSize: { sm: 'sm', md: 'md', lg: 'lg' },
-        size: 'md',
-        src: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png',
+        size: 'lg',
     },
     parameters: {
         design: {
             type: 'figma',
-            url: 'https://www.figma.com/file/jfKRr1V9evJUp1uBeyP3Zz/v1.0.0?type=design&node-id=7853-14315&mode=dev',
+            url: 'https://www.figma.com/file/ISSDryshtEpB7SUSdNqAcw/branch/jfKRr1V9evJUp1uBeyP3Zz/Aragon-ODS?node-id=10413%3A11972&mode=dev',
         },
     },
 };
 
 /**
- * Usage as a wallet address avatar
+ * Wallet address avatar
  */
-export const WalletAvatar: Story = {
+export const Wallet: Story = {
     args: {
         size: 'sm',
         src: '0x0000000000000000000000000000000000000000',
@@ -38,20 +36,40 @@ export const WalletAvatar: Story = {
     parameters: {
         design: {
             type: 'figma',
-            url: 'https://www.figma.com/file/jfKRr1V9evJUp1uBeyP3Zz/v1.0.0?type=design&node-id=7853-14315&mode=dev',
+            url: 'https://www.figma.com/file/ISSDryshtEpB7SUSdNqAcw/branch/jfKRr1V9evJUp1uBeyP3Zz/Aragon-ODS?node-id=7853%3A14199&mode=dev',
         },
     },
 };
 
-export const DaoAvatar: Story = {
+/**
+ * Token symbol avatar
+ */
+export const Token: Story = {
     args: {
-        size: 'lg',
-        copy: 'Patito Dao',
+        responsiveSize: { md: 'md' },
+        size: 'md',
+        src: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png',
     },
     parameters: {
         design: {
             type: 'figma',
-            url: 'https://www.figma.com/file/jfKRr1V9evJUp1uBeyP3Zz/v1.0.0?type=design&node-id=7853-14315&mode=dev',
+            url: 'https://www.figma.com/file/ISSDryshtEpB7SUSdNqAcw/branch/jfKRr1V9evJUp1uBeyP3Zz/Aragon-ODS?type=design&node-id=1-32&mode=design&t=sfIu3NdB5L0zbR2w-0',
+        },
+    },
+};
+
+/**
+ * Fallback Dao avatar
+ */
+export const Dao: Story = {
+    args: {
+        size: 'lg',
+        src: 'Patito Dao',
+    },
+    parameters: {
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/ISSDryshtEpB7SUSdNqAcw/branch/jfKRr1V9evJUp1uBeyP3Zz/Aragon-ODS?node-id=10413%3A11972&mode=dev',
         },
     },
 };
--- src/components/avatars/avatar/avatar.test.tsx
+++ src/components/avatars/avatar/avatar.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { Avatar, type IAvatarProps } from './avatar';
 
@@ -13,17 +13,28 @@ describe('<Avatar /> component', () => {
 
     it('renders the Avatar component', () => {
         render(createTestComponent({ src: etherIcon }));
-        expect(screen.getByTestId('avatar')).toBeInTheDocument();
+
+        const avatarEl = screen.getByAltText('avatar');
+        expect(avatarEl).toBeInTheDocument();
+        expect(avatarEl).toHaveAttribute('src', etherIcon);
     });
 
-    it('renders a fallback with no image when no `src` is provided', () => {
+    it('renders a fallback when no image source is provided', async () => {
         render(createTestComponent());
         expect(screen.getByTestId('fallbackAvatar')).toBeInTheDocument();
     });
 
-    it('renders a fallback with initials when `copy` is provided and `src` is not', () => {
-        render(createTestComponent({ copy: 'Patito Dao' }));
+    it('renders a fallback when source leads to broken image2', async () => {
+        render(createTestComponent({ src: 'https://exxxsample.com/abc.jpg' }));
+
+        fireEvent.error(screen.getByAltText('avatar'));
+
+        expect(await screen.findByTestId('fallbackAvatar')).toBeInTheDocument();
+    });
+
+    it('renders a fallback with initials when src is not a valid image source', () => {
+        render(createTestComponent({ src: 'Aragon Dao' }));
         expect(screen.getByTestId('fallbackAvatar')).toBeInTheDocument();
-        expect(screen.getByText('PD')).toBeInTheDocument();
+        expect(screen.getByText('AD')).toBeInTheDocument();
     });
 });
