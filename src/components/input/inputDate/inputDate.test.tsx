fbfe04227372fe9db7aace3cc4d591603cea04db
Ruggero Cino <ruggero.cino@gmail.com>
2023-12-13 11:25:05 +0100

    Implement input-date tests

--- src/components/input/inputDate/inputDate.test.tsx
+++ src/components/input/inputDate/inputDate.test.tsx
@@ -1,5 +1,30 @@
+import { render, screen, within } from '@testing-library/react';
+import React from 'react';
+import { IconType } from '../../icon';
+import { InputDate, type IInputDateProps } from './inputDate';
+
 describe('<InputDate /> component', () => {
-    it('TODO', () => {
-        // TODO
+    const useRefMock = jest.spyOn(React, 'useRef');
+
+    const createTestComponent = (props?: Partial<IInputDateProps>) => {
+        const completeProps = { ...props };
+
+        return <InputDate {...completeProps} />;
+    };
+
+    it('renders a date input', () => {
+        const label = 'Date label';
+        render(createTestComponent({ label }));
+        const dateInput = screen.getByLabelText<HTMLInputElement>(label);
+        expect(dateInput).toBeInTheDocument();
+        expect(dateInput.type).toEqual('date');
+    });
+
+    it('renders a button to open the date picker on click', () => {
+        useRefMock.mockImplementation(() => ({ current: { showPicker: jest.fn() } }));
+        render(createTestComponent());
+        const calendarButton = screen.getByRole('button');
+        expect(calendarButton).toBeInTheDocument();
+        expect(within(calendarButton).getByTestId(IconType.CALENDAR)).toBeInTheDocument();
     });
 });
