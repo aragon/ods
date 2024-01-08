7be19571be92ca6ef0515ec61b8ec47927a36607
Ruggero Cino <ruggero.cino@gmail.com>
2024-01-08 14:57:05 +0100

    Update testLogger.ts

--- src/test/utils/testLogger.ts
+++ src/test/utils/testLogger.ts
@@ -3,13 +3,15 @@ class TestLogger {
     private shouldSuppressErrors = false;
     private originalConsoleError = console.error;
 
+    private testErrorLogger = jest.fn((...params) => {
+        if (!this.shouldSuppressErrors) {
+            this.originalConsoleError.apply(console, params);
+        }
+    });
+
     setup = () => {
         beforeEach(() => {
-            console.error = jest.fn((...params) => {
-                if (!this.shouldSuppressErrors) {
-                    this.originalConsoleError.apply(console, params);
-                }
-            });
+            console.error = this.testErrorLogger;
         });
 
         afterEach(() => {
