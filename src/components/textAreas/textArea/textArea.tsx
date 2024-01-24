5214ed0e04edd5c321f5f3496ffc2ecb8f85c760
Ruggero Cino <ruggero.cino@gmail.com>
2024-01-24 16:03:21 +0100

    feat: APP-2743 - Implement TextArea component

---
+++ src/components/textAreas/textArea/textArea.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames';
+import { InputContainer, type IInputComponentProps } from '../../input';
+import { useInputProps } from '../../input/hooks';
+
+export interface ITextAreaProps extends IInputComponentProps<HTMLTextAreaElement> {}
+
+export const TextArea: React.FC<ITextAreaProps> = (props: ITextAreaProps) => {
+    const { containerProps, inputProps } = useInputProps(props);
+    const { className: inputClassName, ...otherInputProps } = inputProps;
+
+    return (
+        <InputContainer {...containerProps}>
+            <textarea type="text" className={classNames('min-h-[144px]', inputClassName)} {...otherInputProps} />
+        </InputContainer>
+    );
+};
