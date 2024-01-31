0f180ff7fd148e07ebd8f4976d1c1228386052bc
Ruggero Cino <ruggero.cino@gmail.com>
2024-01-31 15:32:15 +0100

    Set overflow-auto to textarea wrapper to clip scrollbars

--- src/components/textAreas/textArea/textArea.tsx
+++ src/components/textAreas/textArea/textArea.tsx
@@ -6,10 +6,15 @@ export interface ITextAreaProps extends IInputComponentProps<HTMLTextAreaElement
 
 export const TextArea: React.FC<ITextAreaProps> = (props: ITextAreaProps) => {
     const { containerProps, inputProps } = useInputProps(props);
+
     const { className: inputClassName, ...otherInputProps } = inputProps;
+    const { wrapperClassName: containerWrapperClassName, ...otherContainerProps } = containerProps;
 
     return (
-        <InputContainer wrapperClassName="grow" {...containerProps}>
+        <InputContainer
+            wrapperClassName={classNames('grow overflow-auto', containerWrapperClassName)}
+            {...otherContainerProps}
+        >
             <textarea type="text" className={classNames('min-h-[144px]', inputClassName)} {...otherInputProps} />
         </InputContainer>
     );
