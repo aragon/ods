018c9f036c7a1a519250df51e10aea8eb650581d
KD <whomst@kevind.xyz>
2024-02-01 08:43:38 +0100

    chore: remove test component from card

--- src/components/cards/card/card.tsx
+++ src/components/cards/card/card.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import type { HTMLAttributes } from 'react';
-import { EmptyState } from '../../emptyState';
 
 export interface ICardProps extends HTMLAttributes<HTMLDivElement> {}
 
@@ -8,16 +7,6 @@ export const Card: React.FC<ICardProps> = (props) => {
     const { className, ...otherProps } = props;
 
     return (
-        <div className={classNames('rounded-xl border border-neutral-100 bg-neutral-0', className)} {...otherProps}>
-            <EmptyState
-                illustrationType="object"
-                illustration="BUILD"
-                title="Title"
-                description="Description"
-                primaryButton={{ size: 'sm', variant: 'secondary', children: 'Secondary' }}
-                secondaryButton={{ size: 'sm', variant: 'primary', children: 'Secondary' }}
-                isStacked={true}
-            />
-        </div>
+        <div className={classNames('rounded-xl border border-neutral-100 bg-neutral-0', className)} {...otherProps} />
     );
 };
