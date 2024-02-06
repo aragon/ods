75fd3bcf86d6696f18ce5ccbb961e6cd0f5469b4
Ruggero Cino <ruggero.cino@gmail.com>
2024-02-06 18:52:23 +0100

    Update mergeRefs.ts

--- src/utils/mergeRefs/mergeRefs.ts
+++ src/utils/mergeRefs/mergeRefs.ts
@@ -1,5 +1,8 @@
 import type { LegacyRef, MutableRefObject, RefCallback } from 'react';
 
+/**
+ * Utility to merge multiple React refs, inspired by https://github.com/gregberge/react-merge-refs
+ */
 export const mergeRefs = <T = unknown>(
     refs: Array<MutableRefObject<T> | LegacyRef<T> | undefined | null>,
 ): RefCallback<T> => {
