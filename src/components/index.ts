ef57737e3cfa1823647703b80477f1519d07ba55
Ruggero Cino <ruggero.cino@gmail.com>
2024-02-07 14:47:51 +0100

    Fix export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ export * from './alerts';
 export * from './avatars';
 export * from './button';
 export * from './cards';
-export * from './checkboxes';
+export * from './checkbox';
 export * from './icon';
 export * from './illustrations';
 export * from './input';
