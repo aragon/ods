60b7d6eeae57b476443d6741ba1d803fd11d0e9c
Fabrice Francois <fabricevladimir@gmail.com>
2024-02-25 17:43:05 -0500

    add dialogContent setup

--- src/components/dialogs/dialog/index.ts
+++ src/components/dialogs/dialog/index.ts
@@ -1,10 +1,13 @@
-import { DialogHeader } from './dialogHeader/dialogHeader';
+import { DialogContent } from './dialogContent';
+import { DialogHeader } from './dialogHeader';
 import { DialogRoot } from './dialogRoot';
 
 export const Dialog = {
-    Root: DialogRoot,
+    Content: DialogContent,
     Header: DialogHeader,
+    Root: DialogRoot,
 };
 
+export { type IDialogContentProps } from './dialogContent';
 export { type IDialogHeaderProps } from './dialogHeader';
 export { type IDialogRootProps } from './dialogRoot';
