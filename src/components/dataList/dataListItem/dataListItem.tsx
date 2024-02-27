5ea635f365c9d9c0f4a7ff24f2cce4362e001c53
Ruggero Cino <ruggero.cino@gmail.com>
2024-02-27 11:29:48 +0100

    Update dataListItem.tsx

--- src/components/dataList/dataListItem/dataListItem.tsx
+++ src/components/dataList/dataListItem/dataListItem.tsx
@@ -9,19 +9,25 @@ export const DataListItem: React.FC<IDataListItemProps> = (props) => {
 
     const { state, childrenItemCount } = useDataListContext();
 
-    const isLoadingElement = state === 'initialLoading' || (state === 'loading' && childrenItemCount === 0);
+    const isSkeletonElement = state === 'initialLoading' || (state === 'loading' && childrenItemCount === 0);
 
     const actionItemClasses = classNames(
         'rounded-xl border border-neutral-100 bg-neutral-0 px-4 py-3 transition-all', // Default
         'focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset', // Focus state
-        { 'hover:border-neutral-200 hover:shadow-neutral-md active:border-neutral-300': !isLoadingElement }, // Hover states when not loading
-        { 'cursor-pointer': !isLoadingElement }, // Not loading element
+        { 'hover:border-neutral-200 hover:shadow-neutral-md active:border-neutral-300': !isSkeletonElement }, // Hover states when not skeleton
+        { 'cursor-pointer': !isSkeletonElement }, // Not skeleton element
         'md:px-6 md:py-3.5', // Responsive
         className,
     );
 
     return (
-        <a className={actionItemClasses} href={href} aria-hidden={isLoadingElement} {...otherProps}>
+        <a
+            className={actionItemClasses}
+            href={href}
+            aria-hidden={isSkeletonElement}
+            tabIndex={isSkeletonElement ? -1 : 0}
+            {...otherProps}
+        >
             {children}
         </a>
     );
