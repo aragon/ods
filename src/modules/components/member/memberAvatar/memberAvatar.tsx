d18737183436d94d0118d125f9e75280f29a668d
KD <whomst@kevind.xyz>
2024-03-10 20:42:24 +0100

    chore: logic cleanup, semantic naming improvments

--- src/modules/components/member/memberAvatar/memberAvatar.tsx
+++ src/modules/components/member/memberAvatar/memberAvatar.tsx
@@ -7,45 +7,41 @@ import { Avatar, Spinner, type IAvatarProps } from '../../../../core';
 
 export interface IMemberAvatarProps extends Omit<IAvatarProps, 'fallback'> {
     /**
-     * ENS name  of the user to lookup avatar src.
+     * ENS name of the user to lookup avatar src.
      */
     ensName?: string;
     /**
-     * Ethereum 0x address of the user to look up ENS name and avatar src.
+     * 0x address of the user to look up ENS name and avatar src.
      */
     address?: string;
     /**
-     * URL src of the user avatar image to be rendered.
+     * Direct URL src of the user avatar image to be rendered.
      */
     avatarSrc?: string;
-    /**
-     * Loading state of the avatar for external lookups.
-     */
-    isLoading?: boolean;
 }
 
 export const MemberAvatar: React.FC<IMemberAvatarProps> = (props) => {
-    const { ensName, address, avatarSrc, size = 'lg', className, ...otherProps } = props;
+    const { ensName, address, avatarSrc, size = 'sm', className, ...otherProps } = props;
     const isValidAddress = address ? isAddress(address) : false;
-    const isPossibleENSName = ensName ? ensName.length > 4 && ensName.endsWith('.eth') : false;
+    const isValidENSName = ensName ? ensName.length >= 7 && ensName.endsWith('.eth') : false;
 
     const { data: ensAddressData, isLoading: addressLoading } = useEnsAddress({
         name: ensName,
-        query: { enabled: isPossibleENSName },
+        query: { enabled: isValidENSName },
     });
-    const resolvedAddress = isValidAddress ? address : (ensAddressData as Hash);
+    const resolvedAddress = isValidAddress ? address : ensAddressData;
 
     const { data: ensNameData, isLoading: nameLoading } = useEnsName({
         address: resolvedAddress as Hash,
         query: { enabled: !!resolvedAddress },
     });
-    const resolvedName = ensName ?? (ensNameData as string);
+    const resolvedName = isValidENSName ? ensName : ensNameData;
 
-    const { data: ensAvatarSrc, isLoading: avatarLoading } = useEnsAvatar({
-        name: resolvedName,
-        query: { enabled: isPossibleENSName },
+    const { data: ensAvatarData, isLoading: avatarLoading } = useEnsAvatar({
+        name: resolvedName as string,
+        query: { enabled: !!resolvedName },
     });
-    const resolvedAvatarSrc = avatarSrc ? avatarSrc : (ensAvatarSrc as string);
+    const resolvedAvatarSrc = avatarSrc ? avatarSrc : ensAvatarData;
 
     const blockiesSrc = resolvedAddress ? blockies.create({ seed: resolvedAddress }).toDataURL() : undefined;
 
