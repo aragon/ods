cea7cf9d4c5f8d4e4edb5eb4cfa17963470a337b
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-18 09:26:08 -0400

    update percentages for skeleton bar widths

--- src/modules/components/proposal/proposalDataListItem/proposalDataListItemSkeleton/proposalDataListItemSkeleton.tsx
+++ src/modules/components/proposal/proposalDataListItem/proposalDataListItemSkeleton/proposalDataListItemSkeleton.tsx
@@ -12,23 +12,23 @@ export const ProposalDataListItemSkeleton: React.FC<IProposalDataListItemSkeleto
             tabIndex={0}
             aria-busy="true"
             aria-label="loading"
-            className={classNames('flex flex-col gap-y-4 bg-neutral-0 py-4 md:py-5', classNames)}
+            className={classNames('flex flex-col gap-y-4 bg-neutral-500 py-4 md:py-5', classNames)}
             {...otherProps}
         >
             <div className="flex justify-between">
-                <StateSkeletonBar size="lg" responsiveSize={{ md: 'xl' }} width="18%" />
-                <StateSkeletonBar size="lg" responsiveSize={{ md: 'xl' }} width="33.5%" />
+                <StateSkeletonBar size="lg" responsiveSize={{ md: 'xl' }} width="16%" />
+                <StateSkeletonBar size="lg" responsiveSize={{ md: 'xl' }} width="24%" />
             </div>
             <div className="flex flex-col gap-y-2">
-                <StateSkeletonBar size="xl" className="w-44 md:w-96" width="60%" />
+                <StateSkeletonBar size="xl" width="72%" />
                 <div className="flex flex-col gap-y-1 md:gap-y-2">
                     <StateSkeletonBar size="lg" width="100%" />
-                    <StateSkeletonBar size="lg" width="84%" />
+                    <StateSkeletonBar size="lg" width="64%" />
                 </div>
             </div>
             <div className="flex justify-between">
-                <StateSkeletonBar size="lg" responsiveSize={{ md: 'xl' }} width="33.33%" />
-                <StateSkeletonBar size="lg" responsiveSize={{ md: 'xl' }} width="18%" />
+                <StateSkeletonBar size="lg" responsiveSize={{ md: 'xl' }} width="20%" />
+                <StateSkeletonBar size="lg" responsiveSize={{ md: 'xl' }} width="16%" />
             </div>
         </DataList.Item>
     );
