5454e4a75bea94d80c22cc9a5546d065331dd853
Kevin Davis <whomst@kevind.xyz>
2024-05-08 12:26:01 +0200

    chore: rewrite tests for collapsibile card after refactor

--- src/core/components/cards/cardCollapsible/cardCollapsible.test.tsx
+++ src/core/components/cards/cardCollapsible/cardCollapsible.test.tsx
@@ -7,7 +7,7 @@ global.ResizeObserver = class {
     disconnect() {}
 };
 
-describe('<CardCollapsible />', () => {
+describe('<CardCollapsible /> component', () => {
     const createTestComponent = (props?: Partial<ICardCollapsibleProps>) => {
         const completeProps = { ...props };
 
@@ -18,5 +18,16 @@ describe('<CardCollapsible />', () => {
         const children = 'Content of the card';
         render(createTestComponent({ children }));
         expect(screen.getByText('Content of the card')).toBeInTheDocument();
+    });
+
+    it('forwards props to the Collapsible component', () => {
+        const defaultOpen = true;
+        const buttonLabelOpened = 'Close';
+        const buttonLabelClosed = 'Open';
+        jest.spyOn(HTMLElement.prototype, 'scrollHeight', 'get').mockReturnValue(500);
+
+        render(createTestComponent({ buttonLabelOpened, defaultOpen }));
+        expect(screen.queryByText(buttonLabelClosed)).not.toBeInTheDocument();
+        expect(screen.getByText(buttonLabelOpened)).toBeInTheDocument();
     });
 });
