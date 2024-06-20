760725563c62e12dfeb7cff481662ea51dc6c0f1
sepehr sanaee <sepehr2mail@gmail.com>
2024-06-20 15:12:22 +0330

    removed proposalVoteDataListItem

--- src/modules/components/vote/proposalVoteDataListItem/proposalVoteDataListItemSkeleton/index.ts
+++
@@ -1,4 +0,0 @@
-export {
-    VoteProposalDataListItemSkeleton,
-    type IVoteProposalDataListItemSkeletonProps,
-} from './voteProposalDataListItemSkeleton';
--- src/modules/components/vote/proposalVoteDataListItem/proposalVoteDataListItemStructure/index.ts
+++
@@ -1,4 +0,0 @@
-export {
-    VoteProposalDataListItemStructure,
-    type IVoteProposalDataListItemStructureProps,
-} from './VoteProposalDataListItemStructure';
