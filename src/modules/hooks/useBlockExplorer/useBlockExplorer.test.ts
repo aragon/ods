d917b9ea40962ded12c811ec0c207f7e5ca65be8
rcaragon <radzionc@aragon.org>
2024-06-26 14:11:41 +0400

    refactor: Update useBlockExplorer hook to handle missing block explorer URL

--- src/modules/hooks/useBlockExplorer/useBlockExplorer.test.ts
+++ src/modules/hooks/useBlockExplorer/useBlockExplorer.test.ts
@@ -4,8 +4,14 @@ import { mainnet, polygon } from 'wagmi/chains';
 import { useBlockExplorer } from './useBlockExplorer';
 
 describe('useBlockExplorer hook', () => {
+    const useChainsSpy = jest.spyOn(wagmi, 'useChains');
+
+    afterEach(() => {
+        useChainsSpy.mockReset();
+    });
+
     it('generates correct URL for different entity types and chain IDs', () => {
-        jest.spyOn(wagmi, 'useChains').mockReturnValue([mainnet, polygon]);
+        useChainsSpy.mockReturnValue([mainnet, polygon]);
 
         const { result } = renderHook(() => useBlockExplorer());
         expect(result.current.getChainEntityUrl({ type: 'address', chainId: 1, id: '0x123' })).toEqual(
@@ -18,11 +24,27 @@ describe('useBlockExplorer hook', () => {
 
     it('throws an error when block explorer URL is missing', () => {
         const { blockExplorers, ...chainWithoutBlockExplorer } = mainnet;
-        jest.spyOn(wagmi, 'useChains').mockReturnValue([chainWithoutBlockExplorer]);
+        useChainsSpy.mockReturnValue([chainWithoutBlockExplorer]);
 
         const { result } = renderHook(() => useBlockExplorer());
         expect(() => result.current.getChainEntityUrl({ type: 'address', chainId: 1, id: '0x123' })).toThrow(
             'useBlockExplorer: Block explorer URL not found for chain with id 1',
+        );
+    });
+
+    it('uses the first chain set on the wagmi provider when chainId property is not set', () => {
+        useChainsSpy.mockReturnValue([mainnet, polygon]);
+
+        const { result } = renderHook(() => useBlockExplorer());
+        expect(result.current.getChainEntityUrl({ type: 'address', id: '0x123' })).toEqual(
+            'https://etherscan.io/address/0x123',
+        );
+    });
+
+    it('uses the first chain passed as parameters when chainId is not defined', () => {
+        const { result } = renderHook(() => useBlockExplorer({ chains: [polygon, mainnet] }));
+        expect(result.current.getChainEntityUrl({ type: 'address', id: '0x123' })).toEqual(
+            'https://polygonscan.com/address/0x123',
         );
     });
 });
