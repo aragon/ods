885b191cd51141a1aab1b55e9b13d9e9badac0f1
Ruggero Cino <ruggero.cino@gmail.com>
2024-07-01 17:57:20 +0200

    Cleanup tests

--- src/core/components/rerender/rerender.test.tsx
+++ src/core/components/rerender/rerender.test.tsx
@@ -17,35 +17,25 @@ describe('<Rerender /> component', () => {
 
     it('renders the initial time', () => {
         const currentTime = Date.now();
-        render(
-            createTestComponent({
-                children: (time) => <div>Current Time: {time}</div>,
-            }),
-        );
+        const children = (time: number) => <div>Current Time: {time}</div>;
+        render(createTestComponent({ children }));
 
         expect(screen.getByText(`Current Time: ${currentTime}`)).toBeInTheDocument();
     });
 
     it('updates time at the specified interval', () => {
-        render(
-            createTestComponent({
-                children: (time) => <div>Current Time: {time}</div>,
-                intervalDuration: 1000,
-            }),
-        );
+        const intervalDuration = 1000;
+        const children = (time: number) => <div>Current Time: {time}</div>;
+        render(createTestComponent({ children, intervalDuration }));
 
         // Fast-forward time by 1 second
-        act(() => {
-            jest.advanceTimersByTime(1000);
-        });
+        act(() => jest.advanceTimersByTime(intervalDuration));
 
         const updatedTime = Date.now();
         expect(screen.getByText(`Current Time: ${updatedTime}`)).toBeInTheDocument();
 
         // Fast-forward time by another second
-        act(() => {
-            jest.advanceTimersByTime(1000);
-        });
+        act(() => jest.advanceTimersByTime(intervalDuration));
 
         const secondUpdatedTime = Date.now();
         expect(screen.getByText(`Current Time: ${secondUpdatedTime}`)).toBeInTheDocument();
@@ -54,11 +44,7 @@ describe('<Rerender /> component', () => {
     it('clears the interval on unmount', () => {
         const clearIntervalSpy = jest.spyOn(window, 'clearInterval');
 
-        const { unmount } = render(
-            createTestComponent({
-                children: (time) => <div>Current Time: {time}</div>,
-            }),
-        );
+        const { unmount } = render(createTestComponent());
 
         unmount();
 
