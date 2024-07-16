87f9dbd24384c683290bf888d1901285d97f6530
Kevin Davis <whomst@kevind.xyz>
2024-07-16 19:35:54 +0200

    chore: stories for container and action

---
+++ src/modules/components/proposal/proposalActions/proposalActionsAction/proposalActionsAction.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ProposalActions } from '..';
+import { Accordion } from '../../../../../core';
+import { withdrawActionsMock } from '../actions/proposalActionWithdrawToken/mocks';
+import { ProposalActionsProvider } from '../proposalActionsContext';
+
+const meta: Meta<typeof ProposalActions.Action> = {
+    title: 'Modules/Components/Proposal/ProposalActions/ProposalActions.Action',
+    component: ProposalActions.Action,
+    parameters: {
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/design/ISSDryshtEpB7SUSdNqAcw/Aragon-ODS?m=auto&t=aAKsoiPV8GlakDa1-1',
+        },
+    },
+};
+
+type Story = StoryObj<typeof ProposalActions.Action>;
+
+/**
+ * Usage example of the ProposalActions.Action module component with mocked TokenWithdraw actions.
+ */
+export const TokenWithdraw: Story = {
+    args: {
+        action: withdrawActionsMock[0],
+    },
+    render: (props) => (
+        <ProposalActionsProvider>
+            <Accordion.Container isMulti={true}>
+                <ProposalActions.Action {...props} />
+            </Accordion.Container>
+        </ProposalActionsProvider>
+    ),
+};
+
+export default meta;
---
+++ src/modules/components/proposal/proposalActions/proposalActionsContainer/proposalActionsContainer.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ProposalActions, ProposalActionsProvider } from '..';
+import { withdrawActionsMock } from '../actions/proposalActionWithdrawToken/mocks';
+
+const meta: Meta<typeof ProposalActions.Container> = {
+    title: 'Modules/Components/Proposal/ProposalActions/ProposalActions.Container',
+    component: ProposalActions.Container,
+    parameters: {
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/design/ISSDryshtEpB7SUSdNqAcw/Aragon-ODS?m=auto&t=aAKsoiPV8GlakDa1-1',
+        },
+    },
+};
+
+type Story = StoryObj<typeof ProposalActions.Container>;
+
+/**
+ * Usage example of the ProposalActions.Container module component with mocked TokenWithdraw actions.
+ */
+export const TokenWithdraw: Story = {
+    args: {
+        actions: withdrawActionsMock,
+    },
+    render: (props) => (
+        <ProposalActionsProvider>
+            <ProposalActions.Container {...props} />
+        </ProposalActionsProvider>
+    ),
+};
+
+export default meta;
