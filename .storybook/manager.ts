d5a13457813e4309394a5a61dbb831b7400470dd
shan8851 <asamshan456@gmail.com>
2024-08-09 11:57:39 +0100

    chore: naming

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/manager-api';
-import customTheme from './theme';
+import aragonOdsTheme from './theme';
 
 addons.setConfig({
-    theme: customTheme,
+    theme: aragonOdsTheme,
 });
