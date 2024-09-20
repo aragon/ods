1be289430cf704d8b2e2aaa58e8c453d34ace0b4
Ruggero Cino <ruggero.cino@gmail.com>
2024-09-20 10:47:04 +0200

    Fix text-alignment for button data-list items

--- src/core/components/dataList/dataListItem/dataListItem.tsx
+++ src/core/components/dataList/dataListItem/dataListItem.tsx
@@ -19,11 +19,11 @@ export const DataListItem: React.FC<IDataListItemProps> = (props) => {
     const isInteractiveElement = !isSkeletonElement && (isLinkElement || props.onClick != null);
 
     const actionItemClasses = classNames(
-        'w-full rounded-xl border border-neutral-100 bg-neutral-0 px-4 py-3 outline-none transition-all focus:outline-none', // Default
+        'w-full rounded-xl border border-neutral-100 bg-neutral-0 px-4 py-3 text-left outline-none transition-all focus:outline-none', // Default
         { 'focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset': isInteractiveElement }, // Interactive focus state
         { 'hover:border-neutral-200 hover:shadow-neutral-md active:border-neutral-300': isInteractiveElement }, // Interactive hover state
         { 'cursor-pointer': isInteractiveElement }, // Interactive default state
-        { 'cursor-default text-left ': !isInteractiveElement }, // Non-interactive default state
+        { 'cursor-default': !isInteractiveElement }, // Non-interactive default state
         'md:px-6 md:py-3.5', // Responsive
         className,
     );
