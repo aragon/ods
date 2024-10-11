5290ff4e2ba4a591ad76c93a52079f31a2812258
Ruggero Cino <ruggero.cino@gmail.com>
2024-10-11 11:21:23 +0200

    feat(APP-3676): Update ProposalDataListItem component to make proposal type optional

--- src/modules/components/proposal/proposalDataListItem/proposalDataListItemStructure/proposalDataListItemStructure.api.ts
+++ src/modules/components/proposal/proposalDataListItem/proposalDataListItemStructure/proposalDataListItemStructure.api.ts
@@ -1,10 +1,16 @@
-import { type IDataListItemProps } from '../../../../../core';
+import type { IDataListItemProps } from '../../../../../core';
 import { type ICompositeAddress, type IWeb3ComponentProps } from '../../../../types';
 import { type ProposalStatus } from '../../proposalUtils';
 
-export type ProposalType = 'majorityVoting' | 'approvalThreshold';
+export type ProposalType = 'majorityVoting' | 'approvalThreshold' | undefined;
 
-export type IProposalDataListItemStructureBaseProps<TType extends ProposalType = ProposalType> = IDataListItemProps &
+export type ProposalResult<TType extends ProposalType> = TType extends 'majorityVoting'
+    ? IMajorityVotingResult
+    : TType extends 'approvalThreshold'
+      ? IApprovalThresholdResult
+      : undefined;
+
+export type IProposalDataListItemStructureBaseProps<TType extends ProposalType = undefined> = IDataListItemProps &
     IWeb3ComponentProps & {
         /**
          * Proposal id
@@ -25,7 +31,7 @@ export type IProposalDataListItemStructureBaseProps<TType extends ProposalType =
         /**
          * Result of the proposal shown only when it is active, challenged or vetoed.
          */
-        result?: TType extends 'majorityVoting' ? IMajorityVotingResult : IApprovalThresholdResult;
+        result?: ProposalResult<TType>;
         /**
          * Proposal status
          */
@@ -41,7 +47,7 @@ export type IProposalDataListItemStructureBaseProps<TType extends ProposalType =
         /**
          * Type of the ProposalDataListItem
          */
-        type: TType;
+        type?: TType;
         /**
          * Indicates whether the connected wallet has voted
          */
@@ -100,5 +106,6 @@ export interface IMajorityVotingResult extends IProposalResultBase {
 }
 
 export type IProposalDataListItemStructureProps =
+    | IProposalDataListItemStructureBaseProps<undefined>
     | IProposalDataListItemStructureBaseProps<'majorityVoting'>
     | IProposalDataListItemStructureBaseProps<'approvalThreshold'>;
